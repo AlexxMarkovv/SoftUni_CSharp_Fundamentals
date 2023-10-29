aedb1cc16c5e18e71e6e45d73e5acba7959dc7bf
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-29 13:20:33 +0200

    Create Program.cs

---
+++ Practice MORE EXERCISE/08. Sunglasses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _08._Sunglasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = int.Parse(Console.ReadLine());
+
+            int formula = 2 * numb;
+            string asteriks = new string('*', formula);
+            string space = new string(' ', numb);
+
+
+            int straightLine = (numb - 1) / 2 - 1;
+
+            Console.WriteLine("{0}{1}{0}", asteriks, space);
+
+            for (int row = 0; row < numb - 2; row++)
+            {
+                char sumbol = ' ';
+                if (row == straightLine)
+                {
+                    sumbol = '|';
+                }
+                string forwarSlash = new string('/', formula - 2);
+                string middelSpace = new string(sumbol, numb);
+                Console.WriteLine("*{0}*{1}*{0}*", forwarSlash, middelSpace);
+
+            }
+
+            Console.WriteLine("{0}{1}{0}", asteriks, space);
+        }
+    }
+}
