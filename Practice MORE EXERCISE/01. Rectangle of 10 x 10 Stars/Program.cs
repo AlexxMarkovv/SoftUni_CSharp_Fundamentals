539398a130578873cf0bbf34c63510722a3f03ea
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-29 13:16:19 +0200

    t

---
+++ Practice MORE EXERCISE/01. Rectangle of 10 x 10 Stars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Rectangle_of_10_x_10_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
