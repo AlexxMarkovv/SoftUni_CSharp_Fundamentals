88212d82d0bff0d25526e9f1c1b4889da63bc6d9
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-29 13:16:46 +0200

    t

---
+++ Practice MORE EXERCISE/03. Square of Stars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Square_of_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
