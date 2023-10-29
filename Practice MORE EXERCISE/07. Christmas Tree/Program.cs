4d880c595062b56b256fa24219205f7826afbe6c
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-29 13:19:44 +0200

    Create Program.cs

---
+++ Practice MORE EXERCISE/07. Christmas Tree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07._Christmas_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num = byte.Parse(Console.ReadLine());
+
+            for (int row = 0; row <= num; row++)
+            {
+                Console.Write(new string(' ', num - row));
+                Console.Write(new string('*', row));
+                Console.Write(" | ");
+                Console.Write(new string('*', row));
+                Console.WriteLine(new string(' ', num - row));
+            }
+        }
+    }
+}
