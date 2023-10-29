6d3f983b0d9e836de8f84b76c81400e7a349808f
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-29 13:19:04 +0200

    Create Program.cs

---
+++ Practice MORE EXERCISE/06. Rhombus of Stars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06._Rhombus_of_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short number = short.Parse(Console.ReadLine());
+
+            for (int row = 1; row < number; row++)
+            {
+                for (int space = 1; space <= number - row; space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star = 0; star < row; star++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+
+            for (int row = number; row >= 1; row--)
+            {
+                for (int space = number - row; space >= 1; space--)
+                {
+                    Console.Write(" ");
+                }
+                for (int star = row; star >= 1; star--)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
