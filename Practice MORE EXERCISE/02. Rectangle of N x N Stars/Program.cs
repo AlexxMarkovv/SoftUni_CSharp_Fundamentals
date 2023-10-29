37c7a5104a9fd28073b5fcacccc3ea1b09284594
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-29 13:18:46 +0200

    t

---
+++ Practice MORE EXERCISE/02. Rectangle of N x N Stars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Rectangle_of_N_x_N_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
