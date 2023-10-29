b0c1bbdd474a90d9bb7bbb001d1a8f775fda0d63
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-29 13:07:28 +0200

    Task

---
+++ Classes And Objects Exercises/04. Students/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = input[0];
+                string lastName = input[1];
+                double grade = double.Parse(input[2]);
+
+                Student student = new Student(firstName, lastName, grade);
+                students.Add(student);
+            }
+
+            foreach (Student student in students.OrderByDescending(x => x.Grade))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            }
+        }
+
+        class Student
+        {
+            public Student(string firstName, string lastName, double grade)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Grade = grade;
+            }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public double Grade { get; set; }
+        }
+    }
+}
