3d97a7171a6862fddda28e06130712206da30b06
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-29 13:19:26 +0200

    Create Program.cs

---
+++ Practice MORE EXERCISE/04. Triangle of Dollars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _04._Triangle_of_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short number = short.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= number; row++)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    Console.Write("$ ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
