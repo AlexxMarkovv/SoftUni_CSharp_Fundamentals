71b6ace7717d0ddc7583e6f263105ce5f6e72651
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-10-30 23:12:59 +0200

    t

---
+++ EXERCISES/04. Toy Shop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04._Toy_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double excursionPrice = double.Parse(Console.ReadLine());
+            int puzles = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int bears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            double totalSum = (puzles * 2.6) + (dolls * 3.0) + (bears * 4.1) + (minions * 8.2) + (trucks * 2.0);
+            int totalToysSold = puzles + dolls + bears + minions + trucks;
+
+            if (totalToysSold >= 50)
+            {
+                totalSum = totalSum * 0.75;
+            }
+
+            totalSum = totalSum * 0.9;
+
+            if (totalSum >= excursionPrice)
+            {
+                Console.WriteLine($"Yes! {totalSum - excursionPrice:f2} lv left.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Not enough money! {excursionPrice - totalSum:f2} lv needed.");
+            }
+
+
+        }
+    }
+}
---
+++ EXERCISES/05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05._Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int extras = int.Parse(Console.ReadLine());
+            double clothes = double.Parse(Console.ReadLine());
+            double deckor = budget * 0.1;
+            double clothesPrice = extras * clothes;
+
+            if (extras > 150)
+            {
+                clothesPrice = clothesPrice * 0.9;
+            }
+
+            if ((deckor + clothesPrice) > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {((deckor + clothesPrice) - budget):f2} leva more.");
+            }
+
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {(budget - (deckor + clothesPrice)):f2} leva left.");
+            }
+        }
+    }
+}
